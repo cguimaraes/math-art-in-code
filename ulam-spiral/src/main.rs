67f3505d6c084ecaede0151d1039f4b8b8369389
Carlos Guimaraes <carlos.em.guimaraes@gmail.com>
2023-02-24 00:05:44 +0100

    Split SquareMatrix from UlamSpiral structs

--- ulam-spiral/src/main.rs
+++ ulam-spiral/src/main.rs
@@ -22,44 +22,6 @@ struct SquareMatrix {
 }
 
 impl SquareMatrix {
-    pub fn new(size: usize) -> Self {
-        assert!(size > 0 && size % 2 != 0, "Matrix size must be odd and bigger than 0!");
-
-        let mut elems = vec![0; size * size];
-
-        let mut dir: Directions = Directions::RIGHT;
-        let mut step: usize = 1;
-        let mut xy_cursor: (usize, usize) = (size / 2, size / 2);
-        elems[(xy_cursor.1 * size) + xy_cursor.0] = 1;
-
-        let mut i = 2;
-        'outer: loop {
-            for _ in 0..2 { // Step is incremented every two directions
-                for _ in 0..step { // Number of steps before turning direction
-                    if i > (size * size) {
-                        break 'outer;
-                    }
-
-                    match dir {
-                        Directions::RIGHT => xy_cursor.0 += 1,
-                        Directions::UP => xy_cursor.1 -= 1,
-                        Directions::LEFT => xy_cursor.0 -= 1,
-                        Directions::DOWN => xy_cursor.1 += 1,
-                    }
-
-                    elems[(xy_cursor.1 * size) + xy_cursor.0] = i;
-                    i = i + 1;
-                }
-                dir = dir.rotate_counter_clockwise();
-            }
-            step = step + 1;
-        }
-
-        Self {
-            elems
-        }
-    }
-
     pub fn len(&self) -> usize {
         return self.elems.len();
     }
@@ -76,6 +38,53 @@ impl Index<usize> for SquareMatrix {
 impl IndexMut<usize> for SquareMatrix {
     fn index_mut(&mut self, idx: usize) -> &mut usize {
         return &mut self.elems[idx];
+    }
+}
+
+struct UlamSpiral {
+    matrix: SquareMatrix
+}
+
+impl UlamSpiral {
+    pub fn new(size: usize) -> Self {
+        assert!(size > 0 && size % 2 != 0, "Matrix size must be odd and bigger than 0!");
+
+        let mut matrix = SquareMatrix {
+            elems: vec![0; size * size],
+        };
+        
+
+        let mut dir: Directions = Directions::RIGHT;
+        let mut step: usize = 1;
+        let mut xy_cursor: (usize, usize) = (size / 2, size / 2);
+        matrix.elems[(xy_cursor.1 * size) + xy_cursor.0] = 1;
+
+        let mut i = 2;
+        'outer: loop {
+            for _ in 0..2 { // Step is incremented every two directions
+                for _ in 0..step { // Number of steps before turning direction
+                    if i > (size * size) {
+                        break 'outer;
+                    }
+
+                    match dir {
+                        Directions::RIGHT => xy_cursor.0 += 1,
+                        Directions::UP => xy_cursor.1 -= 1,
+                        Directions::LEFT => xy_cursor.0 -= 1,
+                        Directions::DOWN => xy_cursor.1 += 1,
+                    }
+
+                    matrix.elems[(xy_cursor.1 * size) + xy_cursor.0] = i;
+                    i = i + 1;
+                }
+                dir = dir.rotate_counter_clockwise();
+            }
+            step = step + 1;
+        }
+
+        Self {
+            matrix
+        }
     }
 }
 
@@ -112,35 +121,35 @@ fn is_prime(n: usize) -> bool {
     return true;
 }
 
-fn print_matrix(m: &SquareMatrix, as_numbers: bool) {
-    let size: u32 = (m.len() as f32).sqrt() as u32;
+fn print_matrix(us: &UlamSpiral, as_numbers: bool) {
+    let size: u32 = (us.matrix.len() as f32).sqrt() as u32;
 
-    for pos in 0..m.len() as u32 {
+    for pos in 0..us.matrix.len() as u32 {
         print!("{}{}",
-               if as_numbers {m[pos.try_into().unwrap()]} else {if is_prime(m[pos.try_into().unwrap()]) { 1 } else { 0 }},
+               if as_numbers {us.matrix[pos.try_into().unwrap()]} else {if is_prime(us.matrix[pos.try_into().unwrap()]) { 1 } else { 0 }},
                if (pos + 1) % size == 0 {"\n"} else {""});
     }
 }
 
-fn save_as_image(m: &SquareMatrix, path: &str) {
-    let size: u32 = (m.len() as f32).sqrt() as u32;
+fn save_as_image(us: &UlamSpiral, path: &str) {
+    let size: u32 = (us.matrix.len() as f32).sqrt() as u32;
     let mut img: image::GrayImage = ImageBuffer::new(size, size);
 
-    for pos in 0..m.len() as u32 {
+    for pos in 0..us.matrix.len() as u32 {
         img.put_pixel(pos % size,
                       pos / size,
-                      if is_prime(m[pos.try_into().unwrap()]) { Luma([0]) } else { Luma([255]) });
+                      if is_prime(us.matrix[pos.try_into().unwrap()]) { Luma([0]) } else { Luma([255]) });
     }
 
     img.save(path).unwrap();
 }
 
 fn main() {
-    let size: usize = 4001;
-    let verbose: bool = false;
+    let size: usize = 3;
+    let verbose: bool = true;
     let output: &str = "./test.png";
     
-    let matrix = SquareMatrix::new(size);
-    if verbose { print_matrix(&matrix, true); }
-    save_as_image(&matrix, output);
+    let ulam_spiral = UlamSpiral::new(size);
+    if verbose { print_matrix(&ulam_spiral, true); }
+    save_as_image(&ulam_spiral, output);
 }
